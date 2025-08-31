e820a9d740007acc56e28ccb6b137174498ed2d9
OliverDong <oliver.dong@oocl.com>
2025-08-31 12:15:18 +0800

    feat: realize service for common api

--- src/main/java/com/example/todo/service/TodoService.java
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -1,8 +1,10 @@
 package com.example.todo.service;
 
 import com.example.todo.dao.TodoDao;
+import com.example.todo.dao.TodoDb;
 import com.example.todo.dao.TodoMemory;
 import com.example.todo.entity.Todo;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,26 +12,33 @@ import java.util.List;
 @Service
 public class TodoService {
     private final TodoDao todoDao;
-    public TodoService(TodoMemory todoDao){
+    public TodoService(TodoDb todoDao){
         this.todoDao=todoDao;
     }
 
     public List<Todo> getTodoList(String title) {
-        return null;
+        return todoDao.getTodoLists(title);
     }
 
     public Todo getTodoById(int id) {
-        return null;
+        return todoDao.getTodoById(id);
     }
-
+    @Transactional
     public Todo newTodo(Todo todo) {
-        return null;
+        todo.setId(null);
+        return todoDao.save(todo);
     }
 
     public Todo updateTodoById(int id, Todo todo) {
-        return null;
+        Todo todoById = todoDao.getTodoById(id);
+        if(todoById==null) throw new IllegalArgumentException("Todo index invalid !");
+        todo.setId(id);
+        return todoDao.save(todo);
     }
 
     public void deleteTodoById(int id) {
+        Todo todoById = todoDao.getTodoById(id);
+        if(todoById==null) throw new IllegalArgumentException("Todo index invalid !");
+        todoDao.deleteTodoById(id);
     }
 }
