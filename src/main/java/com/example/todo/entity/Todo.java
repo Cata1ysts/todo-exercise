040dad932d63d15d9b138c75de9467fef4c3dd4e
OliverDong <oliver.dong@oocl.com>
2025-08-31 09:19:51 +0800

    feat: create entity Todo

--- src/main/java/com/example/todo/entity/Todo.java
+++ src/main/java/com/example/todo/entity/Todo.java
@@ -1,4 +1,42 @@
 package com.example.todo.entity;
 
 public class Todo {
+    private Integer id;
+    private String title;
+    private String status;
+
+    public Todo() {
+
+    }
+
+    public Todo(Integer id, String titles, String status) {
+        this.status = status;
+        this.id = id;
+        this.title = titles;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String titles) {
+        this.title = titles;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
